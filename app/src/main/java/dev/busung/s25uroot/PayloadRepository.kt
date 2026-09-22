8c15e3ef51ff722fc9b51520e83ad023528c4ef0
stainless1028 <exeok77@gmail.com>
2026-09-22 14:47:52 +0900

    Point app and payload repository at stainless1028 fork

--- app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
+++ app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
@@ -137,9 +137,9 @@ class PayloadRepository(private val context: Context) {
 
     companion object {
         private const val COMMIT_API_URL =
-            "https://api.github.com/repos/iamdumdum1234/Root-My-Galaxy-Payloads/git/ref/heads/feature/sm-x610-xsp"
+            "https://api.github.com/repos/stainless1028/Root-My-Galaxy-Payloads/git/ref/heads/feature/sm-x610-xsp"
         private const val RAW_REPOSITORY =
-            "https://raw.githubusercontent.com/iamdumdum1234/Root-My-Galaxy-Payloads"
+            "https://raw.githubusercontent.com/stainless1028/Root-My-Galaxy-Payloads"
         private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/feature/sm-x610-xsp"
         private const val MAX_COMMIT_RESPONSE_BYTES = 16 * 1024
         private const val MAX_MANIFEST_BYTES = 256 * 1024
